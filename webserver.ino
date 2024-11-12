3450665def8759c77628dd341cdbd8b09420847e
Jetinho <jet.julien@gmail.com>
2024-11-12 13:24:45 +0100

    Use AsyncWebServer

--- webserver.ino
+++ webserver.ino
@@ -1,125 +1,27 @@
-// Load Wi-Fi library
 #include <WiFi.h>
-#include "LittleFS.h"
+#include <ESPAsyncWebServer.h>
 
-// Replace with your network credentials
 const char *ssid = "feather32";
 const char *password = "feather32";
 
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
+AsyncWebServer server(80);
 void setup()
 {
   Serial.begin(115200);
-
-  if (!LittleFS.begin())
-  {
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
-  }
-
-  File file = LittleFS.open("/test_example.txt", "r");
-  if (!file)
-  {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.println("File Content:");
-  while (file.available())
-  {
-    Serial.write(file.read());
-  }
-  file.close();
-
+  // Create access point
   Serial.println("\n[*] Creating AP");
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
   Serial.print("[+] AP Created with IP Gateway ");
   Serial.println(WiFi.softAPIP());
 
+  // Define a route to serve the HTML page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+	   Serial.println("ESP32 Web Server: New request received:");  // for debugging
+	   Serial.println("GET /");        // for debugging
+	   request->send(200, "text/html", "<html><body><h1>Hello, ESP32!</h1></body></html>"); });
+  // Start the server
   server.begin();
 }
-
-void loop()
-{
-  WiFiClient client = server.available(); // Listen for incoming clients
-
-  if (client)
-  { // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client."); // print a message out in the serial port
-    String currentLine = "";       // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime)
-    { // loop while the client's connected
-      currentTime = millis();
-      if (client.available())
-      {                         // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        Serial.write(c);        // print it out the serial monitor
-        header += c;
-        if (c == '\n')
-        { // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0)
-          {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // Read the content of index.html file
-            // split content into lines and print ln to client
-            File indexFile = LittleFS.open("/index.html", "r");
-            if (!indexFile)
-            {
-              Serial.println("Failed to open index.html for reading");
-              return;
-            }
-
-            while (indexFile.available())
-            {
-              String line = indexFile.readStringUntil('\n');
-              client.println(line);
-            }
-            indexFile.close();
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-}
+void loop() {}
