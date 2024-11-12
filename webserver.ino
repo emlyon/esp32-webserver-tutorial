26ac20d3f7661ecfec20946d305f1d750c2ecf91
Jetinho <jet.julien@gmail.com>
2024-11-12 13:28:58 +0100

    Functional webserver using LittleFS to serve html file

--- webserver.ino
+++ webserver.ino
@@ -1,14 +1,39 @@
+// Load Wi-Fi library
 #include <WiFi.h>
+#include "LittleFS.h"
 #include <ESPAsyncWebServer.h>
 
+// Replace with your network credentials
 const char *ssid = "feather32";
 const char *password = "feather32";
 
+// Set web server port number to 80
 AsyncWebServer server(80);
+
 void setup()
 {
   Serial.begin(115200);
-  // Create access point
+
+  if (!LittleFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+
+  File file = LittleFS.open("/test_example.txt", "r");
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.println("File Content:");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  file.close();
+
   Serial.println("\n[*] Creating AP");
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
@@ -18,10 +43,12 @@ void setup()
   // Define a route to serve the HTML page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-	   Serial.println("ESP32 Web Server: New request received:");  // for debugging
-	   Serial.println("GET /");        // for debugging
-	   request->send(200, "text/html", "<html><body><h1>Hello, ESP32!</h1></body></html>"); });
+    Serial.println("ESP32 Web Server: New request received:"); // for debugging
+    Serial.println("GET /");                                   // for debugging
+    request->send(LittleFS, "/index.html", "text/html"); });
+
   // Start the server
   server.begin();
 }
+
 void loop() {}
